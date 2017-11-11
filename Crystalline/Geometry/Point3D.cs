17ea38c9949dee11c5138b09723c4ab0f962bd5a
Michael Miller <icy.arctic.fox@gmail.com>
2017-11-11 13:28:05 -0700

    Add 3D variants of point structs

---
+++ Crystalline/Geometry/Point3D.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Crystalline.Geometry
+{
+    /// <summary>
+    /// Three-dimensional point.
+    /// X, Y, and Z coordinate values are represented as floating-point numbers.
+    /// </summary>
+    public struct Point3D : IEquatable<Point3D>
+    {
+        /// <summary>
+        /// X-coordinate of the point.
+        /// </summary>
+        public double X { get; }
+
+        /// <summary>
+        /// Y-coordinate of the point.
+        /// </summary>
+        public double Y { get; }
+        
+        /// <summary>
+        /// Z-coordinate of the point.
+        /// </summary>
+        public double Z { get; }
+
+        /// <summary>
+        /// Creates a new three-dimensional point.
+        /// </summary>
+        /// <param name="x">Value of the x-coordinate.</param>
+        /// <param name="y">Value of the y-coordinate.</param>
+        /// <param name="z">Value of the z-coordinate.</param>
+        public Point3D(double x, double y, double z)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates a string representation of the point.
+        /// </summary>
+        /// <returns>Point in the format: (x, y, z)</returns>
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Compares this point to another to determine whether they are the same.
+        /// </summary>
+        /// <param name="other">Point to compare against.</param>
+        /// <returns>True if the points are equal, false otherwise.</returns>
+        public bool Equals(Point3D other)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Determines whether this instance is equal to another.
+        /// </summary>
+        /// <param name="obj">Other instance to compare against.</param>
+        /// <returns>True if the other instance is a point with the same values.</returns>
+        public override bool Equals(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Computes a hash code from the point's values.
+        /// </summary>
+        /// <returns>Hash code.</returns>
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks whether two points are equal.
+        /// </summary>
+        /// <param name="left">First point to compare.</param>
+        /// <param name="right">Second point to compare.</param>
+        /// <returns>True if the points are identical, false otherwise.</returns>
+        public static bool operator ==(Point3D left, Point3D right)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks whether two points are different.
+        /// </summary>
+        /// <param name="left">First point to compare.</param>
+        /// <param name="right">Second point to compare.</param>
+        /// <returns>True if the points are different, or false if they're identical.</returns>
+        public static bool operator !=(Point3D left, Point3D right)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Crystalline/Geometry/Point3I.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Crystalline.Geometry
+{
+    /// <summary>
+    /// Three-dimensional point.
+    /// X, Y, and Z coordinate values are represented as integers.
+    /// </summary>
+    /// <remarks>This structure is ideal for tracking positions and depth in user interfaces.</remarks>
+    public struct Point3I : IEquatable<Point3I>
+    {
+        /// <summary>
+        /// X-coordinate of the point.
+        /// </summary>
+        public int X { get; }
+
+        /// <summary>
+        /// Y-coordinate of the point.
+        /// </summary>
+        public int Y { get; }
+
+        /// <summary>
+        /// Y-coordinate of the point.
+        /// </summary>
+        public int Z { get; }
+
+        /// <summary>
+        /// Creates a new three-dimensional point.
+        /// </summary>
+        /// <param name="x">Value of the x-coordinate.</param>
+        /// <param name="y">Value of the y-coordinate.</param>
+        /// <param name="z">Value of the z-coordinate.</param>
+        public Point3I(int x, int y, int z)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates a string representation of the point.
+        /// </summary>
+        /// <returns>Point in the format: (x, y, z)</returns>
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Compares this point to another to determine whether they are the same.
+        /// </summary>
+        /// <param name="other">Point to compare against.</param>
+        /// <returns>True if the points are equal, false otherwise.</returns>
+        public bool Equals(Point3I other)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Determines whether this instance is equal to another.
+        /// </summary>
+        /// <param name="obj">Other instance to compare against.</param>
+        /// <returns>True if the other instance is a point with the same values.</returns>
+        public override bool Equals(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Computes a hash code from the point's values.
+        /// </summary>
+        /// <returns>Hash code.</returns>
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks whether two points are equal.
+        /// </summary>
+        /// <param name="left">First point to compare.</param>
+        /// <param name="right">Second point to compare.</param>
+        /// <returns>True if the points are identical, false otherwise.</returns>
+        public static bool operator ==(Point3I left, Point3I right)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks whether two points are different.
+        /// </summary>
+        /// <param name="left">First point to compare.</param>
+        /// <param name="right">Second point to compare.</param>
+        /// <returns>True if the points are different, or false if they're identical.</returns>
+        public static bool operator !=(Point3I left, Point3I right)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
