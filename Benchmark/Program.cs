4d66a5eaa294f86f0fbe7507feed77b39a41418c
Michael Miller <icy.arctic.fox@gmail.com>
2017-11-25 11:37:05 -0700

    Cleanup and improve benchmark
    I'm unsure now about which is the fastest method.

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Benchmark
 {
@@ -7,7 +8,9 @@ namespace Benchmark
     {
         Foo,
 
-        Bar
+        Bar,
+        
+        Baz
     }
 
     public interface IType
@@ -35,134 +38,157 @@ namespace Benchmark
         }
     }
 
+    public class Baz : IType
+    {
+        public TypeEnum Type => TypeEnum.Baz;
+
+        public string baz()
+        {
+            return "baz";
+        }
+    }
+
     internal class Program
     {
+        private const int Iterations = 50_000_000;
+        private const int Rounds = 3;
+        
+        private static string Test1(IType[] types)
+        {
+            string result = null;
+            for (var i = 0; i < Iterations; ++i)
+            {
+                for (var j = 0; j < types.Length; ++j)
+                {
+                    var type = types[j];
+                    switch (type.Type)
+                    {
+                        case TypeEnum.Foo:
+                            result = ((Foo) type).foo();
+                            break;
+                        case TypeEnum.Bar:
+                            result = ((Bar) type).bar();
+                            break;
+                        case TypeEnum.Baz:
+                            result = ((Baz) type).baz();
+                            break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
+            }
+            return result;
+        }
+        
+        private static string Test2(IType[] types)
+        {
+            string result = null;
+            for (var i = 0; i < Iterations; ++i)
+            {
+                for (var j = 0; j < types.Length; ++j)
+                {
+                    var type = types[j];
+                    switch (type)
+                    {
+                        case Foo f:
+                            result = f.foo();
+                            break;
+                        case Bar b:
+                            result = b.bar();
+                            break;
+                        case Baz b:
+                            result = b.baz();
+                            break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
+            }
+            return result;
+        }
+        
+        private static string Test3(IType[] types)
+        {
+            string result = null;
+            for (var i = 0; i < Iterations; ++i)
+            {
+                for (var j = 0; j < types.Length; ++j)
+                {
+                    var type = types[j];
+                    var fo = type as Foo;
+                    if (fo != null)
+                        result = fo.foo();
+                    else
+                    {
+                        var ba = type as Bar;
+                        if (ba != null)
+                            result = ba.bar();
+                        else
+                        {
+                            var bz = type as Baz;
+                            if (bz != null)
+                                result = bz.baz();
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+        
+        private static string Test4(IType[] types)
+        {
+            string result = null;
+            for (var i = 0; i < Iterations; ++i)
+            {
+                for (var j = 0; j < types.Length; ++j)
+                {
+                    var type = types[j];
+                    if (type is Foo fo)
+                        result = fo.foo();
+                    else
+                    {
+                        if (type is Bar ba)
+                            result = ba.bar();
+                        else
+                        {
+                            if (type is Baz bz)
+                                result = bz.baz();
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        delegate string TestDelegate(IType[] types);
+        
         public static void Main(string[] args)
         {
             var types = new IType[]
             {
-                new Foo(), new Bar()
+                new Foo(), new Bar(), new Baz(), new Bar(), new Foo(), new Baz(),
+                new Baz(), new Bar(), new Bar(), new Baz(), new Foo(), new Foo()
             };
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-            string result = null, result2 = null;
-            for (var i = 0; i < 50_000_000; ++i)
+            string result;
+            var tests = new TestDelegate[] {Test1, Test2, Test3, Test4};
+            var results = new[] {0d, 0d, 0d, 0d};
+            for (var i = 0; i < Rounds; ++i)
             {
-                var type = types[0];
-                switch (type.Type)
+                for(var j = 0; j < tests.Length; ++j)
                 {
-                    case TypeEnum.Foo:
-                        result = ((Foo) type).foo();
-                        break;
-                    case TypeEnum.Bar:
-                        result = ((Bar) type).bar();
-                        break;
-                    default:
-                        throw new NotImplementedException();
+                    var test = tests[j];
+                    var stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    result = test(types);
+                    stopwatch.Stop();
+                    Console.WriteLine($"Round {i+1}, Test {j+1}: {stopwatch.Elapsed} - {result}");
+                    results[j] += stopwatch.Elapsed.TotalMilliseconds;
                 }
-                type = types[1];
-                switch (type.Type)
-                {
-                    case TypeEnum.Foo:
-                        result2 = ((Foo) type).foo();
-                        break;
-                    case TypeEnum.Bar:
-                        result2 = ((Bar) type).bar();
-                        break;
-                    default:
-                        throw new NotImplementedException();
-                }
+                Console.WriteLine();
             }
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.Elapsed);
-            Console.WriteLine(result + result2);
-            // ---------------------------
-            stopwatch = new Stopwatch();
-            stopwatch.Start();
-            for (var i = 0; i < 50_000_000; ++i)
-            {
-                var type = types[0];
-                switch (type)
-                {
-                    case Foo f:
-                        result = f.foo();
-                        break;
-                    case Bar b:
-                        result = b.bar();
-                        break;
-                    default:
-                        throw new NotImplementedException();
-                }
-                type = types[1];
-                switch (type)
-                {
-                    case Foo f:
-                        result2 = f.foo();
-                        break;
-                    case Bar b:
-                        result2 = b.bar();
-                        break;
-                    default:
-                        throw new NotImplementedException();
-                }
-            }
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.Elapsed);
-            Console.WriteLine(result + result2);
-            // ---------------------------
-            stopwatch = new Stopwatch();
-            stopwatch.Start();
-            for (var i = 0; i < 50_000_000; ++i)
-            {
-                var type = types[0];
-                var fo = type as Foo;
-                if (fo != null)
-                    result = fo.foo();
-                else
-                {
-                    var ba = type as Bar;
-                    if (ba != null)
-                        result = ba.bar();
-                }
-                type = types[1];
-                fo = type as Foo;
-                if (fo != null)
-                    result2 = fo.foo();
-                else
-                {
-                    var ba = type as Bar;
-                    if (ba != null)
-                        result2 = ba.bar();
-                }
-            }
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.Elapsed);
-            Console.WriteLine(result + result2);
-            // ---------------------------
-            stopwatch = new Stopwatch();
-            stopwatch.Start();
-            for (var i = 0; i < 50_000_000; ++i)
-            {
-                var type = types[0];
-                if (type is Foo fo)
-                    result = fo.foo();
-                else
-                {
-                    if (type is Bar ba)
-                        result = ba.bar();
-                }
-                type = types[1];
-                if (type is Foo fo2)
-                    result2 = fo2.foo();
-                else
-                {
-                    if (type is Bar ba)
-                        result2 = ba.bar();
-                }
-            }
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.Elapsed);
-            Console.WriteLine(result + result2);
+            var min = results.Min();
+            for(var i = 0; i < tests.Length; ++i)
+                Console.WriteLine($"Test {i+1} Avg: {results[i]/Rounds}, Ratio: {results[i]/min}");
         }
     }
 }
