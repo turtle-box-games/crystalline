2ed13a794be38108a53451005e77c34d14933d38
Michael Miller <icy.arctic.fox@gmail.com>
2017-11-25 23:11:03 -0700

    Add methods for converting between lines and vectors

--- Crystalline/Geometry/LineExtensions.cs
+++ Crystalline/Geometry/LineExtensions.cs
@@ -1,7 +1,27 @@
-﻿namespace Crystalline.Geometry
+﻿using System;
+
+namespace Crystalline.Geometry
 {
     public static class LineExtensions
     {
-        
+        public static Vector2D ToVector(this Line2D line)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3D ToVector(this Line3D line)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Line2D ToLine(this Vector2D vector)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Line3D ToLine(this Vector3D vector)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
