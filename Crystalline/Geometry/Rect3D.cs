5cac290962d21a854b7741efcbde41283dcb4f32
Michael Miller <icy.arctic.fox@gmail.com>
2017-11-25 23:16:59 -0700

    Add method declarations for Rect3D and Rect3I

--- Crystalline/Geometry/Rect3D.cs
+++ Crystalline/Geometry/Rect3D.cs
@@ -1,7 +1,59 @@
-﻿namespace Crystalline.Geometry
+﻿using System;
+
+namespace Crystalline.Geometry
 {
-    public struct Rect3D : IPrimitive
+    public struct Rect3D : IPrimitive, IEquatable<Rect3D>
     {
+        public double X { get; }
         
+        public double Y { get; }
+        
+        public double Z { get; }
+        
+        public double Width { get; }
+        
+        public double Height { get; }
+        
+        public double Depth { get; }
+
+        public Rect3D(double x, double y, double z, double width, double height, double depth)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Rect3D(Point3D topFrontLeft, Point3D bottomBackRight)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool Equals(Rect3D other)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public override bool Equals(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static bool operator ==(Rect3D left, Rect3D right)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static bool operator !=(Rect3D left, Rect3D right)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Crystalline/Geometry/Rect3I.cs
+++ Crystalline/Geometry/Rect3I.cs
@@ -1,7 +1,59 @@
-﻿namespace Crystalline.Geometry
+﻿using System;
+
+namespace Crystalline.Geometry
 {
-    public struct Rect3I : IPrimitive
+    public struct Rect3I : IPrimitive, IEquatable<Rect3I>
     {
+        public int X { get; }
         
+        public int Y { get; }
+        
+        public int Z { get; }
+        
+        public int Width { get; }
+        
+        public int Height { get; }
+        
+        public int Depth { get; }
+
+        public Rect3I(int x, int y, int z, int width, int height, int depth)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Rect3I(Point3D topFrontLeft, Point3D bottomBackRight)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool Equals(Rect3I other)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public override bool Equals(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static bool operator ==(Rect3I left, Rect3I right)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static bool operator !=(Rect3I left, Rect3I right)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
